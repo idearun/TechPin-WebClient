2731e8fdd1a08a8b139f8c2dca7375f7e32d3e33
Hamid Tondkaran <htondkar@gmail.com>
2017-09-04 11:38:46 +0430

    makes landing page images lazy loaded

--- src/components/top25/StartUpWidget.js
+++ src/components/top25/StartUpWidget.js
@@ -1,52 +1,60 @@
-import React, {PropTypes} from 'react';
-import {Link} from 'react-router';
-import {baseUrl} from '../../api/realApi';
+import React, { PropTypes } from "react";
+import { Link } from "react-router";
+import { baseUrl } from "../../api/realApi";
+import LazyLoad from "react-lazy-load";
+import StarRating from "../sharedComponents/StarRating";
 
-import StarRating from '../sharedComponents/StarRating';
+import { ListItem } from "material-ui/List";
+import Divider from "material-ui/Divider";
 
-import {ListItem} from 'material-ui/List';
-import Divider from 'material-ui/Divider';
-
-const StartUpWidget = ({product, i}) => {
-  const {name_en, average_p_rate, details, slug} = product;
-
-  const styles = {
-    logo: {
-      width: 85,
-    },
-    container: {
-      backgroundColor: 'white',
-    },
+const styles = {
+  logo: {
+    width: 85
+  },
+  container: {
+    backgroundColor: "white"
   }
-  const generateShortDesc = (desc) => {
-    if (desc.length === 0) {
-      return 'No descriptions yet. be the first one to add descriptions to this product !'
-    } else {
-      let shortDesc = desc.split(" ").splice(0, 10).join(" ")
-      if (shortDesc.split(' ').length < 10) return shortDesc;
-      return `${shortDesc} ...`
-    }
+};
+
+const generateShortDesc = desc => {
+  if (desc.length === 0) {
+    return "No descriptions yet. be the first one to add descriptions to this product !";
+  } else {
+    let shortDesc = desc
+      .split(" ")
+      .splice(0, 10)
+      .join(" ");
+    if (shortDesc.split(" ").length < 10) return shortDesc;
+    return `${shortDesc} ...`;
   }
+};
+
+const LzLoImage = ({ img }) => (
+  <LazyLoad width={85} offsetVertical={50}>
+    <img src={img} alt="logo" style={styles.logo} />
+  </LazyLoad>
+);
+
+const StartUpWidget = ({ product, i }) => {
+  const { name_en, average_p_rate, details, slug } = product;
+
   return (
     <div style={styles.container}>
       <Link to={`${slug}`}>
-        <ListItem
-          className='widget'
-          secondaryTextLines={2}
-          leftAvatar={<img src={baseUrl + details.logo} alt='logo' style={styles.logo} />}
-          primaryText={null}
-          secondaryText={null}>
-            <div className='widget-title'>{name_en}</div>
-            <div className='widget-text'>{generateShortDesc(details.description_en)}</div>
+        <div className="widget">
+          <LzLoImage img={baseUrl + details.logo} />
+          <div>
+            <div className="widget-title">{name_en}</div>
+            <div className="widget-text">
+              {generateShortDesc(details.description_en)}
+            </div>
             <StarRating rating={average_p_rate} />
-        </ListItem>
-        {i < 24 ? <Divider inset={true} /> : ''}
+          </div>
+        </div>
+        {i < 24 ? <Divider inset={true} /> : ""}
       </Link>
     </div>
   );
-};
-
-StartUpWidget.propTypes = {
 };
 
 export default StartUpWidget;
