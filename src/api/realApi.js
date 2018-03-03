70d4b5ad97eb3c02fc8334c75a65b6f735b06c63
Hasan Noori <xishma@gmail.com>
2018-03-03 12:55:23 +0330

    update api server address and build

--- src/api/realApi.js
+++ src/api/realApi.js
@@ -6,7 +6,7 @@ const CancelToken = axios.CancelToken;
 require("core-js/fn/object/values");
 require("core-js/fn/object/entries");
 
-export var baseApiUrl = "https://api.techpin.xyz";
+export var baseApiUrl = "http://api.techpin.ir";
 // const baseApiUrl = 'http://172.25.18.117:8000'
 
 var config = {
