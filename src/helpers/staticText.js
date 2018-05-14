927b4a532ce22cf8f52562dd21462aa46befe886
idearun Startup Studio <info@idearun.co>
2018-05-14 17:57:37 +0430

    Update staticText.js

--- src/helpers/staticText.js
+++ src/helpers/staticText.js
@@ -1,6 +1,3 @@
-const footerText = `With Techpin you can discover the best new startups and products,
-   every day. Its a place for product-loving enthusiasts to share and find out about
-   the latest mobile apps, websites, hardware projects, and tech creations in Iran,
-   in every industry. Best part is you can share your own expirience and rate them.`;
+const footerText = `With Techpin you can discover the best new startups and products, every day. Its a place for product-loving enthusiasts to share and find out about the latest mobile apps, websites, hardware projects, and tech creations in middle east region. Techpin helps VCs and angel investors make the right decisions and invest on the most promising startups in MENA.`;
 
 export {footerText};
